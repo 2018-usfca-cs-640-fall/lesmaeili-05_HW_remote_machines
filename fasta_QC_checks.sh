a9a109dda4a3966c5a75ef78cf69d0e86c316758
Leila Esmaeili <lesmaeili@dons.usfca.edu>
2018-10-04 03:12:13 +0000

    half way done with script

---
+++ fasta_QC_checks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Leila Esmaeili
+# lesmaeili@dons.usfca.edu
+# HW5
+
+DATE=`date +%Y-%m-%d`
+NAME=$DATE"_fasta_QC_log.txt"
+echo "Output from preliminary quality control checks on selected fasta files" >> $NAME
+echo `date` >> $NAME #Creates the Date
+echo >> $NAME #Creates a blank line
+echo "Output for each fasta includes:" >> $NAME  
+echo " Filename, file sie, first three lines, last three lines," >> $NAME
+echo "the number of sequences in the file, and the " >> $NAME
+echo "sequence identifier lines stored alphabetically." >> $NAME
+
+
+for  f in ./data/*.fasta
+do
+	echo >> $NAME
+	echo  "#######################################" >> $NAME
+	echo >> $NAME
+	echo "Output for"$f >> $NAME
+	echo >> $NAME
+done
