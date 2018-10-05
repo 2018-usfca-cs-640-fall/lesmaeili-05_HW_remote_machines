6342fd817c65acec5a7e5df216c8e1ad74830549
Leila Esmaeili <lesmaeili@dons.usfca.edu>
2018-10-05 21:01:57 +0000

    Update script to remove >>

--- fasta_QC_checks.sh
+++ fasta_QC_checks.sh
@@ -4,38 +4,35 @@
 # HW5
 
 DATE=`date +%Y-%m-%d`
-NAME=$DATE"_fasta_QC_log.txt"
 echo "Output from preliminary quality control checks on selected fasta files" >> $NAME
-echo `date` >> $NAME #Creates the Date
-echo >> $NAME #Creates a blank line
-echo "Output for each fasta includes:" >> $NAME  
-echo " Filename, file sie, first three lines, last three lines," >> $NAME
-echo "the number of sequences in the file, and the " >> $NAME
-echo "sequence identifier lines stored alphabetically." >> $NAME
+date # Creates the Date
+echo # Creates a blank line
+echo "Output for each fasta includes:"
+echo "Filename, file size, first three lines, last three lines,"
+echo "the number of sequences in the file, and the"
+echo "sequence identifier lines stored alphabetically."
 
 
-for  f in ./data/*.fasta
+for f in data/*.fasta
 do
-	echo >> $NAME
-	echo  "#######################################" >> $NAME
-	echo >> $NAME
-	echo "Output for"$f >> $NAME
-	echo >> $NAME
-	set -- $(du -h $f)
-	echo "Size: "$1 >> $NAME
-	echo >> $NAME
-	echo  "First three Lines" >>$NAME
-	echo  "------------------">> $NAME
-	echo $(head -3 $f)>> $NAME
-	echo >>$NAME
-	echo "Last three lines:">> $NAME
-	echo "------------------" >> $NAME
-	echo $(tail -3 $f)>> $NAME
-	echo >> $NAME 
-	echo "number of sequences: "$(grep -c "^>" $f) >> $NAME
-	echo >> $NAME
-	echo "Sequence identifier lines (sorted)" >> $NAME
-	echo " place holder for when I figure out how to do this" >> $NAME
-	 
-		
+	echo
+	echo  "#######################################"
+	echo
+	echo Output for "$f"
+	echo
+	echo Size: "$(du -h "$f" | cut -f 1)"
+	echo
+	echo "First three Lines"
+	echo "------------------"
+	head -3 $f
+	echo
+	echo "Last three lines:"
+	echo "------------------"
+	tail -3 $f
+	echo
+	echo number of sequences\: "$(grep -c "^>" "$f")"
+	echo
+	echo "Sequence identifier lines (sorted)"
+	echo " place holder for when I figure out how to do this"
 done
+
